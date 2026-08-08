f8f4aec22b45b6ce36c09043be33230ee8aa1cf8
Jalo Hiltunen <jaloveikhilt@gmail.com>
2026-08-08 12:42:21 +0300

    idk finishing the game, still would add more features
    idk it sucks

--- Projects/coin-dash/coin.gd
+++ Projects/coin-dash/coin.gd
@@ -4,3 +4,17 @@ var screensize = Vector2.ZERO
 
 func pickup():
 	queue_free()
+
+func _ready():
+	$Timer.start(randf_range(3, 8))
+
+
+func _on_timer_timeout() -> void:
+	$AnimatedSprite2D.frame = 0
+	$AnimatedSprite2D.play()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("obstacles"):
+		position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
+		
--- Projects/coin-dash/scenes/player.gd
+++ Projects/coin-dash/scenes/player.gd
@@ -7,14 +7,6 @@ signal hurt
 var velocity = Vector2.ZERO
 var screensize = Vector2(480, 720)
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	set_process(true)
-	position = screensize / 2
-	$AnimatedSprite2D.animation = "idle"
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -29,10 +21,14 @@ func _process(delta: float) -> void:
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	
+func start():
+	set_process(true)
+	position = screensize / 2
+	$AnimatedSprite2D.animation = "idle"	
+	
 func die():
 	$AnimatedSprite2D.animation = "hurt"
 	set_process(false)
-
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("coins"):
