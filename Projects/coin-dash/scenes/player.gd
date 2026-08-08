56a0daf6ff3ba8711d72e30d3fd89355c9526d41
Jalo Hiltunen <jaloveikhilt@gmail.com>
2026-08-08 09:51:21 +0300

    Finalizing the player scene for now
    idk rly

--- Projects/coin-dash/scenes/player.gd
+++ Projects/coin-dash/scenes/player.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+signal pickup
+signal hurt
+
 @export var speed = 350
 var velocity = Vector2.ZERO
 var screensize = Vector2(480, 720)
@@ -7,7 +10,9 @@ var screensize = Vector2(480, 720)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_process(true)
+	position = screensize / 2
+	$AnimatedSprite2D.animatino = "idle"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,4 +22,22 @@ func _process(delta: float) -> void:
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
 	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 	
+func die():
+	$AnimatedSprite2D.animation = "hurt"
+	set_process(false)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("coins"):
+		area.pickup()
+		pickup.emit()
+	if area.is_in_group("obstacles"):
+		hurt.emit()
+		die()
